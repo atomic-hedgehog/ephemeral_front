8124505819af8f1d7588cbc9f10d125191e981fe
Eric Gesell <gesell.eric@gmail.com>
2013-08-10 19:16:20 -0500

    added routes and mocks for dashboard pages

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [hiccup "1.0.4"]
                  [compojure "1.1.5"]]
   :plugins [[lein-ring "0.8.5"]]
   :ring {:handler ephemeral_front.handler/app}
--- src/ephemeral_front/handler.clj
+++ src/ephemeral_front/handler.clj
@@ -1,10 +1,55 @@
 (ns ephemeral_front.handler
-  (:use compojure.core)
+  (:use compojure.core
+        hiccup.core)
   (:require [compojure.handler :as handler]
             [compojure.route :as route]))
 
+(defn upload-form []
+    (html [:form {:action "/jobs" :method "post" :enctype "multipart/form-data"}
+           [:p "Job Name" [:br]
+            [:input {:name "job-name" :type "text"}]]
+           [:p "Source Data File" [:br]
+            [:input {:name "job-data" :size "30" :type "file"}]] 
+           [:br]
+           [:input {:type "submit" :name "submit" :value "Submit Job"}]]))
+
+(defn create-job []
+  (html [:h3 "Job created"]))
+
+;; TODO refactor mock job data into a test framework
+(def mock-job-data {:1 {:id :1 :name "Count the frequencies of words in Moby Dick"} 
+                    :2 {:id :2 :name "Compile a list of emails containing the word 'vacation'"}})
+
+(defn redirect-to [location]
+  {:status 302
+   :headers {"location" location}})
+
+(defn build-job-anchor [job-id]
+  [:li [:a {:href (str "jobs/" (:id (mock-job-data job-id)))} (:name (mock-job-data job-id))]])
+
+(defn show-job-index []
+  (html
+    [:ul
+     (map #(build-job-anchor (:id %)) (vals mock-job-data))]))
+
+(defn show-job-status [id]
+  (html
+    [:h3 (str "status for " id)]))
+  
+
+
+
+
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] 
+       (upload-form))
+  (POST "/jobs" []
+        (create-job)
+        (redirect-to "/jobs"))
+  (GET "/jobs" []
+       (show-job-index))
+  (GET "/jobs/:id" [id]
+       (show-job-status id))
   (route/resources "/")
   (route/not-found "Not Found"))
 
